101fa5cff6f21710933169d718dba12739e25ba1
harry <b558663@my.leedscitycollege.ac.uk>
2023-03-27 15:40:08 +0100

    Log in styling

--- src/app/LogIn/page.tsx
+++ src/app/LogIn/page.tsx
@@ -1,66 +1,78 @@
 "use client";
 
-import { useState, useEffect } from 'react';
-import PocketBase from 'pocketbase';
-import Cookies from 'js-cookie';
-import Link from 'next/link';
+import { useState, useEffect } from "react";
+import PocketBase from "pocketbase";
+import Cookies from "js-cookie";
+import Link from "next/link";
 
-const pb = new PocketBase('http://127.0.0.1:8090');
-const pbCollection = 'user';
+const pb = new PocketBase("http://127.0.0.1:8090");
+const pbCollection = "user";
+const inputformatText = "my-4 py-3 border-2 text-center rounded-2xl px-[60px]";
+const inputformatButton = "my-4 py-3 border-2 text-center rounded-2xl w-[150px]";
 
 export default function LoginPage() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [userData, setUserData] = useState<any | null>(null);
 
   useEffect(() => {
-    const storedUserData = Cookies.get('userData');
+    const storedUserData = Cookies.get("userData");
     if (storedUserData) {
       const parsedUserData = JSON.parse(storedUserData);
       pb.collection(pbCollection)
         .getOne(parsedUserData.id)
-        .then((data:any) => {
+        .then((data: any) => {
           setUserData(data);
-          Cookies.set('userData', JSON.stringify(data));
+          Cookies.set("userData", JSON.stringify(data));
         });
     }
   }, []);
 
-  const handleLogin = async (e:any) => {
+  const handleLogin = async (e: any) => {
     e.preventDefault();
     try {
-      const data = await pb.collection(pbCollection).authWithPassword(email, password);
-      setUserData(data.record);      
+      const data = await pb
+        .collection(pbCollection)
+        .authWithPassword(email, password);
+      setUserData(data.record);
       console.log(JSON.stringify(data.record));
-      Cookies.set('userData', JSON.stringify(data.record));
+      Cookies.set("userData", JSON.stringify(data.record));
     } catch (error) {
       console.error(error);
     }
   };
 
-  console.log(userData) 
+  console.log(userData);
 
   return (
-    <>
-      <form onSubmit={handleLogin}>
+    <div className="h-[calc(100vh-150px)] flex justify-center items-center">
+      <form className="my-100 flex flex-col -mt-[150px] justify-center items-center" onSubmit={handleLogin}>
+        <h1 className="text-4xl">Log In</h1>
         <input
+          className={inputformatText}
           type="email"
           placeholder="Email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
         <input
+          className={inputformatText}
           type="password"
           placeholder="Password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button type="submit">Login</button>
+        <button
+          className={inputformatButton} 
+          type="submit"
+        >
+          Login
+        </button>
       </form>
 
       {userData && <div>Logged in as {userData.firstName}</div>}
 
       {/* <Link href="/Register">Regiser</Link> */}
-    </>
+    </div>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
-import './globals.css'
-import {Navbar} from './Navbar'
-import {Footer} from './Footer'
+import "./globals.css";
+import { Navbar } from "./Navbar";
+import { Footer } from "./Footer";
 
 // export const metadata = {
 //   title: 'Create Next App',
@@ -10,17 +10,25 @@ import {Footer} from './Footer'
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
+      <header>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          rel="stylesheet"
+        ></link>
+      </header>
       <body>
-        <Navbar/>
+        <Navbar />
         <div className="pt-[75px] flex text-center justify-center items-center">
-        {children}
+          {children}
         </div>
-        <Footer/>
+        <Footer />
       </body>
     </html>
-  )
+  );
 }
