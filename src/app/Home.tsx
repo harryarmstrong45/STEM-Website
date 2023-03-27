0b32838450dc10f01c476917dd7926eec63e656c
harryarmstrong45 <94044306+harryarmstrong45@users.noreply.github.com>
2023-03-27 12:03:49 +0000

    More Content Again

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "./page.module.css";
+import { useRouter } from "next/router";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,7 +11,7 @@ export default function Home() {
       <header>
         <title>Homepage</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
           rel="stylesheet"
---
+++ src/app/createAccount.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function createAccount() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
